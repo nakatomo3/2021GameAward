b33523ba44326b6ea146676a99400e28736a3a17
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2021-02-23 10:22:08 +0900

    SystemSupportクラスにデバッグ便利機能追加

--- Assets/Scripts/Systems/SystemSupporter.cs
+++ Assets/Scripts/Systems/SystemSupporter.cs
@@ -2,13 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class SystemSupporter {
 	public static void ExitGame() {
-		#if UNITY_EDITOR
-			EditorApplication.isPlaying = false;
-		#endif
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
 	}
 
-	
+	public static void PlaySupport() {
+#if UNITY_EDITOR
+		if (Input.GetKeyDown(KeyCode.Return)){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			Time.timeScale = 3;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift)) {
+			Time.timeScale = 1;
+		}
+#endif
+	}
+
 }
