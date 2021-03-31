e4426b79bd9a7deb6091da4ccfcd36c3dc3b35e9
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2021-03-31 16:29:13 +0900

    プレイヤーカメラを少し手触り改善

--- Assets/Scripts/Stage/Player/PlayerCamera.cs
+++ Assets/Scripts/Stage/Player/PlayerCamera.cs
@@ -15,15 +15,19 @@ public class PlayerCamera : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		deltaPosition = Vector3.Lerp(deltaPosition, Vector3.zero, 0.03f);
+		deltaPosition = Vector3.Lerp(deltaPosition, Vector3.zero, 0.02f);
 
-		deltaPosition += new Vector3(InputManager.GetAxis(Keys.R_HORIZONTAL), 0, InputManager.GetAxis(Keys.R_VERTICAL)).normalized * 10 * Time.deltaTime;
+		var moveVector = new Vector3(InputManager.GetAxis(Keys.R_HORIZONTAL), 0, InputManager.GetAxis(Keys.R_VERTICAL));
+		deltaPosition += moveVector.normalized * 10 * Time.deltaTime;
 		
 		if (Mathf.Abs(deltaPosition.x) > maxRange) {
 			deltaPosition = new Vector3(Mathf.Sign(deltaPosition.x) * maxRange, 0, deltaPosition.z);
 		}
 		if(Mathf.Abs(deltaPosition.z) > maxRange) {
 			deltaPosition = new Vector3(deltaPosition.x, 0, Mathf.Sign(deltaPosition.z) * maxRange);
+		}
+		if(moveVector.magnitude < 0.1f) {
+			deltaPosition = Vector3.Lerp(deltaPosition, Vector3.zero, 0.1f);
 		}
 
 		camera.transform.position = gameObject.transform.position + new Vector3(0, 10, -5) + deltaPosition;
