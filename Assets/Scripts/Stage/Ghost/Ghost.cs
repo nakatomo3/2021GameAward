ae1395778494f2557ccb626b8185735dbd191ee9
sugimotokento <keyunutoma@gmail.com>
2021-04-30 18:42:50 +0900

    ゴーストの視線を3にした

--- Assets/Scripts/Stage/Ghost/Ghost.cs
+++ Assets/Scripts/Stage/Ghost/Ghost.cs
@@ -21,55 +21,59 @@ public class Ghost : MonoBehaviour {
     }
 
     protected bool CheckViewPlayer(Vector3 viewDirection, Vector3 playerDistance) {
-        //Vector3 distance = playerDistance;
-        //Vector3 direction = Vector3.Normalize(distance);
-        //float len = Mathf.Pow(Mathf.Pow(distance.x, 2) + Mathf.Pow(distance.y, 2) + Mathf.Pow(distance.z, 2), 0.5f);
-
-        ////プレイヤーとの距離のベクトルと正面方向のベクトルの間の角度を求める
-        //float dot = Vector3.Dot(direction, viewDirection);
-        //float angle = Mathf.Acos(dot);
-
-        //bool isIntoView = Mathf.Abs(angle) < viewAngle / 180 * 3.14 && len < viewDist;
-        //bool isViewForward = viewDirection == direction && len < viewDist;
-        //if (isIntoView == true || isViewForward == true) {
-        //    Vector3 playerPos = Player.instance.transform.position;
-        //    Vector3 thisPos = this.transform.position;
-
-
-        //    float loopAdd = 1 / viewDist;
-        //    for (float j = loopAdd; j < 1; j += loopAdd) {
-        //        Vector3 temp = Vector3.Lerp(thisPos, playerPos, j);
-        //        temp.x = Mathf.Round(temp.x);
-        //        temp.y = Mathf.Round(temp.y);
-        //        temp.z = Mathf.Round(temp.z);
-
-        //        GameObject obj = Stage.instance.GetStageObject(temp);
-
-        //        //壁に隠れているか判定する
-        //        if (obj != null) {
-        //            if (obj.name[0] == '2') {
-        //                return false;
-
-        //            } else if (obj.name[0] == '3') {
-        //                return false;
-        //            }
-        //        }
-        //        //スタート地点も見えない
-        //        if (temp.x == Mathf.Round(Player.instance.startPosition.x) && temp.z == Mathf.Round(Player.instance.startPosition.z)) {
-        //            return false;
-        //        }
-        //    }
-
-        //    Player.instance.GhostGameOver();
-        //    return true;
-        //}
-
+        //プレイヤーと同じ位置にいる
         if (this.transform.position == Player.instance.transform.position) {
             if (this.transform.position != Stage.instance.startPosition) {
                 Player.instance.GhostGameOver();
                 return true;
             }
         }
+
+        //視線方向にいるか
+        Vector3 distance = playerDistance;
+        Vector3 direction = Vector3.Normalize(distance);
+        float len = Mathf.Pow(Mathf.Pow(distance.x, 2) + Mathf.Pow(distance.y, 2) + Mathf.Pow(distance.z, 2), 0.5f);
+
+        //プレイヤーとの距離のベクトルと正面方向のベクトルの間の角度を求める
+        float dot = Vector3.Dot(direction, viewDirection);
+        float angle = Mathf.Acos(dot);
+
+        bool isIntoView = Mathf.Abs(angle) < viewAngle / 180 * 3.14 && len < viewDist;
+        bool isViewForward = viewDirection == direction && len < viewDist;
+        if (isIntoView == true || isViewForward == true) {
+            Vector3 playerPos = Player.instance.transform.position;
+            Vector3 thisPos = this.transform.position;
+
+
+            float loopAdd = 1 / viewDist;
+            for (float j = loopAdd; j < 1; j += loopAdd) {
+                Vector3 temp = Vector3.Lerp(thisPos, playerPos, j);
+                temp.x = Mathf.Round(temp.x);
+                temp.y = Mathf.Round(temp.y);
+                temp.z = Mathf.Round(temp.z);
+
+                GameObject obj = Stage.instance.GetStageObject(temp);
+
+                //壁に隠れているか判定する
+                if (obj != null) {
+                    if (obj.name[0] == '2') {
+                        return false;
+
+                    } else if (obj.name[0] == '3') {
+                        return false;
+                    }
+                }
+                //スタート地点も見えない
+                if (temp.x == Mathf.Round(Player.instance.startPosition.x) && temp.z == Mathf.Round(Player.instance.startPosition.z)) {
+                    return false;
+                }
+            }
+
+            Player.instance.GhostGameOver();
+            return true;
+        }
+
+    
 
         return false;
     }
