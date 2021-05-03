c38e00c147cbd29dbeb11cf4378a3bf4b989bdf9
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2021-05-03 15:27:09 +0900

    スイッチのバグ修正

--- Assets/Scripts/Stage/Objects/Switch.cs
+++ Assets/Scripts/Stage/Objects/Switch.cs
@@ -5,71 +5,65 @@ using System.Text;
 
 public class Switch : ChannelBase {
 
-    [SerializeField]
-    private GameObject switchOn;
-    [SerializeField]
-    private GameObject switchOff;
+	[SerializeField]
+	private GameObject switchOn;
+	[SerializeField]
+	private GameObject switchOff;
 
-    private bool isUnderPlayer;
+	private bool isUnderPlayer;
 
-    // Start is called before the first frame update
-    void Start() {
-        switchOn.SetActive(false);
-        switchOff.SetActive(true);
-    }
+	// Start is called before the first frame update
+	void Start() {
+		switchOn.SetActive(false);
+		switchOff.SetActive(true);
+	}
 
-    // Update is called once per frame
-    void Update() {
-        if (this.transform.position == Player.instance.transform.position && isUnderPlayer == false) {
-            if (SwitchManager.instance.channel[channel] == true) {
-                SwitchManager.instance.channel[channel] = false;
-                switchOn.SetActive(false);
-                switchOff.SetActive(true);
+	// Update is called once per frame
+	void Update() {
+		switchOn.SetActive(SwitchManager.instance.channel[channel]);
+		switchOff.SetActive(!SwitchManager.instance.channel[channel]);
 
-            } else {
-                SwitchManager.instance.channel[channel] = true;
-                switchOn.SetActive(true);
-                switchOff.SetActive(false);
-            }
-            isUnderPlayer = true;
-        } else if (transform.position != Player.instance.transform.position) {
-            isUnderPlayer = false;
-        }
+		if (this.transform.position == Player.instance.transform.position && isUnderPlayer == false) {
+			SwitchManager.instance.channel[channel] = !SwitchManager.instance.channel[channel];
+			isUnderPlayer = true;
+		} else if (transform.position != Player.instance.transform.position) {
+			isUnderPlayer = false;
+		}
 
-    }
+	}
 
-    public override string ToFileString() {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("A_Switch");
-        sb.AppendLine("pos:" + Mathf.CeilToInt(transform.position.x) + "," + Mathf.CeilToInt(transform.position.z));
-        sb.AppendLine("channel:" + channel);
-        return sb.ToString();
-    }
+	public override string ToFileString() {
+		StringBuilder sb = new StringBuilder();
+		sb.AppendLine("A_Switch");
+		sb.AppendLine("pos:" + Mathf.CeilToInt(transform.position.x) + "," + Mathf.CeilToInt(transform.position.z));
+		sb.AppendLine("channel:" + channel);
+		return sb.ToString();
+	}
 
-    public override string ToEditorString() {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("チャンネル：　" + channel);
-        return sb.ToString();
-    }
+	public override string ToEditorString() {
+		StringBuilder sb = new StringBuilder();
+		sb.AppendLine("チャンネル：　" + channel);
+		return sb.ToString();
+	}
 
-    public override void SetData(string information) {
-        var options = information.Split('\n');
-        for (int i = 0; i < options.Length; i++) {
-            var option = options[i];
-            if (option.Contains(":")) {
-                var key = option.Split(':')[0];
-                var value = option.Split(':')[1];
-                switch (key) {
-                    case "channel":
-                        channel = uint.Parse(value);
-                        break;
-                }
-            }
-        }
-        return;
-    }
+	public override void SetData(string information) {
+		var options = information.Split('\n');
+		for (int i = 0; i < options.Length; i++) {
+			var option = options[i];
+			if (option.Contains(":")) {
+				var key = option.Split(':')[0];
+				var value = option.Split(':')[1];
+				switch (key) {
+					case "channel":
+						channel = uint.Parse(value);
+						break;
+				}
+			}
+		}
+		return;
+	}
 
-    public override void Action() {
-        
-    }
+	public override void Action() {
+
+	}
 }
