d4247161a68a4076f3da01bae52c627a2021e80e
Snow0420 <zero84yuki1309@gmail.com>
2021-05-13 10:52:29 +0900

    ダメージブロックをトゲブロックに差し替え

--- Assets/Scripts/Stage/Objects/DamageField.cs
+++ Assets/Scripts/Stage/Objects/DamageField.cs
@@ -4,43 +4,19 @@ using UnityEngine;
 
 public class DamageField : MonoBehaviour {
 
-    [SerializeField]
-    private Renderer render;
-
     private bool isDamage = true;
-    private float count = 0.0f; 
-    private float interval = 1.0f;
 
     // Start is called before the first frame update
     void Start() {
-        render = GetComponentInChildren<Renderer>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (transform.position == Player.instance.transform.position) {
-            if (isDamage == false) {
-                count += Time.deltaTime;
-                if (count >= interval) { // インターバルカウント
-                    count = 0.0f;
-                    isDamage = true;
-                    Color color = render.material.color;
-                    color.g = 1.0f;
-                    render.material.color = color;
-                }
-            } else {
-                Player.instance.Damage(1); // ダメージ処理
-                isDamage = false;
-                Color color = render.material.color;
-                color.g = 0.0f;
-                render.material.color = color;
-            }
+        if (transform.position == Player.instance.transform.position && isDamage == true) {
+            Player.instance.Damage(1); // ダメージ処理
+            isDamage = false;
         } else { // プレイヤーが離れたらリセット
-            count = 0.0f;
             isDamage = true;
-            Color color = render.material.color;
-            color.g = 1.0f;
-            render.material.color = color;
         }
     }
 }
