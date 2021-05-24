91b45d9de4916229484c2c2b9a387068a0c8c1ac
Mamo Futaba <48075059+jadurdice@users.noreply.github.com>
2021-05-24 13:05:15 +0900

    サウンドファイル、スクリプト一式仮実装 内部組み込みは今日授業中やりあす（特にマネージャーはまた修正してない）

---
+++ Assets/Sound/Script/AudioElement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+/// <summary>
+/// 基本のオーディオユニットを定義するサウンドのクラス
+/// </summary>
+[System.Serializable]
+public class AudioElement
+{
+        #region インスペクタ編集部
+    //By Brackeys
+    //https://www.youtube.com/watch?v=6OT43pvUyfY
+
+    [Tooltip("名前")]
+    public string name;
+    [Tooltip("サウンドファイル")]
+    public AudioClip clip;
+    [Tooltip("音量")]
+    [Range(0f, 1f)]
+    public float volume;
+    [Tooltip("ピッチ")]
+    [Range(.1f,3f)]
+    public float pitch;
+    [Tooltip("ループフラグ")]
+    public bool loop;
+    [HideInInspector]
+    public AudioSource source;
+        #endregion
+}
---
+++ Assets/Sound/Script/AudioManeger.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManeger : MonoBehaviour
+{
+    //オーディオコントロールスクリプト
+    //Original by Brackeys
+    //https://www.youtube.com/watch?v=6OT43pvUyfY
+
+    public AudioElement[] sounds;
+    public static AudioManeger instance;
+    public bool[] fadeOutFlag;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (AudioElement s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop; 
+        }
+    }
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (fadeOutFlag[i])
+            {
+                FadeOut(i);
+            }
+        }
+    }
+
+    AudioElement SearchSound(string name){
+        AudioElement s = Array.Find(sounds, AudioElement => AudioElement.name == name);
+        if(s==null)
+        {
+            Debug.Log("AudioElement:" + name + " not found!");
+            return null;
+        }
+return s;
+    }
+
+    public void Play(string name)
+    {
+        AudioElement s = SearchSound(name);
+        if(SearchSound(name)==null){
+            return;
+        }
+        s.source.Play();
+    }
+
+        public void Play(AudioElement sound)
+    {
+        sound.source.Play();
+    }
+
+    //音量をコントロールする関数
+    public void SetVolume(string name, float value)
+    {
+        AudioElement s = SearchSound(name);
+        if(SearchSound(name)==null){
+            return;
+        }
+        s.source.volume = value;
+Play(s);
+    }
+
+    //ピッチをコントロールする関数
+    public void SetPitch(string name,float value)
+    {
+        AudioElement s = SearchSound(name);
+        if(SearchSound(name)==null){
+            return;
+        }
+        s.source.pitch = value;
+        Play(s);
+    }
+
+    //再生中のサウンドをフェードインやフェードアウトさせる関数
+    void Fade(string name,bool isFadeOut,float second)
+    {
+AudioElement s = SearchSound(name);
+        StartCoroutine(FadeAudio(s,isFadeOut,second));
+    }
+
+public void StopAllSound(){
+    
+}
+
+    IEnumerator FadeAudio(AudioElement sound,bool isFadeOut,float second){
+        float t = 0.0f;
+        while (t < second)
+        {
+            if(!isFadeOut){     //フェード院... 「私も同行する」
+    sound.source.volume = Mathf.Lerp(0.0f,1.0f,t/second);
+            }else{              //フェードアウト
+    sound.source.volume = Mathf.Lerp(1.0f,0.0f,t/second);
+            }
+            t+=Time.deltaTime;
+                    yield return null;
+        }
+
+
+
+    }
+}
