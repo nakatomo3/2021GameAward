aa3b39573e85a16d485066279f2e3c66ba75cd12
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2021-05-29 23:00:25 +0900

    壁の見た目的な確率修正

--- Assets/Scripts/Stage/Objects/Wall.cs
+++ Assets/Scripts/Stage/Objects/Wall.cs
@@ -12,5 +12,10 @@ public class Wall : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
 		renderer.material = materials[Stage.instance.visualMode];
+        var rand = Random.Range(0, 4);
+        if(Random.Range(0, 5) > 1) {
+            rand = 0;
+        }
+        renderer.material.mainTextureOffset = new Vector2(rand * 0.25f, 0);
 	}
 }
